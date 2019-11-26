cf39a07549f50e6594d17392493bb708eda7303c
unnamed <>
2019-11-26 15:16:37 +0100

    Receive and Broadcasr Message via Socket
    Receive messages via sockets and send it back.
    (without saving to databse)

--- server/socket/ioConnection.js
+++ server/socket/ioConnection.js
@@ -1,16 +1,22 @@
 const logger = require('../../logger/pino');
 
 const ioConnectionCallback = socket => {
-	logger.debug("-----SOCKET-----New client connected socket.id: %s", socket.id);
+	logger.debug("-----SOCKET----- New client connected socket.id: %s", socket.id);
 	socket.on("disconnect", () => {
-		logger.debug("-----SOCKET-----Client disconnected socket.id: %s", socket.id);
+		logger.debug("-----SOCKET----- Client disconnected socket.id: %s", socket.id);
 	});
 
+	socket.on("new-message", data => {
+		logger.debug("-----SOCKET----- on new-message, %O", data);
+		socket.broadcast.emit("new-message", { ...data, message: { ...data.message, id: 9999 } });
+		socket.emit("new-message-completed", { ...data, message: { ...data.message, id: 9999 } });
+	});
 
-	socket.emit("FromAPI", { DateTime: new Date().toUTCString() });
+	socket.emit("connected", { DateTime: new Date().toUTCString() });
 
 	socket.on("activeRoom", (roomId) => {
-		console.log('roomId', roomId)
+		console.log('roomId', roomId);
+		
 		socket.emit("activeRoomUpdated", {roomId});
 	})
 };
